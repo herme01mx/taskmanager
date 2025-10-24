19fd6bdaae5c4074f2ef4e8db13d5e3dd5a839e5
herme01mx <hambriz@hotmail.com>
2025-10-23 18:48:38 -0600

    Persistencia en json agregada

--- task_manager.py
+++ task_manager.py
@@ -1,3 +1,7 @@
+import json
+
+
+
 class Task:
     
     def __init__(self, id, description, completed=False):
@@ -11,15 +15,20 @@ class Task:
     
     
 class TaskManager:
+    
+    FILENAME="tasks.json"
+    
     def __init__(self):
         self._tasks = [] #Lista vacia de tareas
         self._next_id = 1  #Contador privado para asignar IDs únicos a las tareas
+        self.load_tasks()
 
     def add_task(self, description):
         task = Task(self._next_id, description)
         self._tasks.append(task)
         self._next_id += 1
         print(f"Tarea añadida: {description}")
+        self.save_tasks()
         
     
     def lst_tasks(self):
@@ -37,6 +46,7 @@ class TaskManager:
                 if task.id == task_id:
                     task.completed = True
                     print(f"Tarea #{id} marcada como completada.")
+                    self.save_tasks()
                     return
             except ValueError:
                 print("ID inválido. Por favor ingrese un número entero.")
@@ -46,11 +56,48 @@ class TaskManager:
     
     def delete_task(self, id):
         for task in self._tasks:
-            if task.id == id:
-                self._tasks.remove(task)
-                print(f"Tarea #{id} eliminada.")
+            try:
+                task_id = int(id)
+                if task.id == task_id:
+                    self._tasks.remove(task)
+                    print(f"Tarea #{id} eliminada.")
+                    self.save_tasks()
+                    return
+            except ValueError:
+                print("ID inválido. Por favor ingrese un número entero.")
                 return
             
         print(f"Tarea #{id} no encontrada.")
         
         
+    
+    def load_tasks(self):
+        try:
+            with open(self.FILENAME, 'r') as file:
+                data = json.load(file)
+            self._tasks = [Task(item['id'], item['description'], item['completed']) for item in data]
+            if self._tasks:
+                self._next_id = self._tasks[-1].id +1
+            else:
+                self._next_id = 1       
+        except FileNotFoundError:
+            self._tasks = []
+            self._next_id = 1
+        except json.JSONDecodeError:
+            print("Error al cargar las tareas. El archivo puede estar corrupto.")
+            self._tasks = []
+            self._next_id = 1
+
+    def save_tasks(self):
+        try:
+            with open(self.FILENAME, 'w') as file:
+                json.dump([{"id": task.id, "description": task.description, "completed": task.completed} 
+                           for task in self._tasks], file, indent=4)              
+              
+        except FileNotFoundError:
+            self._tasks = []
+            self._next_id = 1
+        except json.JSONDecodeError:
+            print("Error al cargar las tareas. El archivo puede estar corrupto.")
+            self._tasks = []
+            self._next_id = 1
